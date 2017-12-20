14ab0db18f2ffb735155b610a7a354fc3538b214
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2017-12-20 15:34:52 +0100

    update databse, little test fix

--- make_test/codeplea-genann_test.c
+++ make_test/codeplea-genann_test.c
@@ -25,10 +25,10 @@ int __attribute__ ((noinline)) test_harness(void) {
         genann_train(ann, input[1], output + 1, 3);
         genann_train(ann, input[2], output + 2, 3);
         genann_train(ann, input[3], output + 3, 3);
-        genann_train(ann, input[3], output + 4, 4);
-        genann_train(ann, input[3], output + 5, 5);
-        genann_train(ann, input[3], output + 6, 6);
-        genann_train(ann, input[3], output + 7, 7);
+        genann_train(ann, input[4], output + 4, 4);
+        genann_train(ann, input[5], output + 5, 5);
+        genann_train(ann, input[6], output + 6, 6);
+        genann_train(ann, input[7], output + 7, 7);
     }
 
     genann_free(ann);
