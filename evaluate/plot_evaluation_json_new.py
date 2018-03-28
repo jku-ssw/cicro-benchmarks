2fc056f512e809aca30ffa90304cfaeadd510aed
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2018-03-28 12:17:26 +0200

    Some cleanup of plotting script

--- evaluate/plot_evaluation_json_new.py
+++ evaluate/plot_evaluation_json_new.py
@@ -42,26 +42,26 @@ def barplot(ax, dpoints):
     
     @see: http://emptypipes.org/2013/11/09/matplotlib-multicategory-barchart/
     '''
-    
+
     # Aggregate the conditions and the categories according to their
     # mean values
     conditions = [(c, np.mean(dpoints[dpoints[:,0] == c][:,2].astype(float))) 
                   for c in np.unique(dpoints[:,0])]
     categories = [(c, np.mean(dpoints[dpoints[:,1] == c][:,2].astype(float))) 
                   for c in np.unique(dpoints[:,1])]
-    
+
     # sort the conditions, categories and data so that the bars in
     # the plot will be ordered by category and condition
     conditions = [c[0] for c in sorted(conditions, key=o.itemgetter(1))]
     categories = [c[0] for c in sorted(categories, key=o.itemgetter(1))]
-    
+
     dpoints = np.array(sorted(dpoints, key=lambda x: categories.index(x[1])))
 
     # the space between each set of bars
     space = 0.3
     n = len(conditions)
     width = (1 - space) / (len(conditions))
-    
+
     # Create a set of bars at each position
     for i,cond in enumerate(conditions):
         indeces = range(1, len(categories)+1)
@@ -70,18 +70,16 @@ def barplot(ax, dpoints):
         pos = [j - (1 - space) / 2. + i * width for j in indeces]
         ax.bar(pos, vals, yerr=errors, width=width, label=cond, 
                color=cm.Accent(float(i) / n))
-    
+
     # Set the x-axis tick labels to be equal to the categories
     ax.set_xticks(indeces)
     ax.set_xticklabels(categories)
-    plt.setp(plt.xticks()[1], rotation=90)
-    
+    plt.setp(plt.xticks()[1], rotation=45, ha='right')
+
     # Add the axis labels
     ax.set_ylabel("Relative Runtime")
     #ax.set_xlabel("Structure")
-    
-    ax.set_xticklabels(x, rotation=45, ha='right')
-    
+
     # Add a legend
     handles, labels = ax.get_legend_handles_labels()
     ax.legend(handles[::-1], labels[::-1], loc='upper left')
@@ -93,18 +91,12 @@ def barplot(ax, dpoints):
 if __name__ == "__main__":
     bench_results = parse_file(OUTPUT_FILE)
 
-    y = {}
-    y_error = {}
-    x = []
-    
-    dpoints_raw = []
-
     # init list of compilers
-    for key, value in sorted(bench_results.items()):
-        for compiler in sorted(value):
-            if compiler not in y:
-                y[compiler] = []
-                y_error[compiler] = []
+    compilers = set()
+    for _, value in sorted(bench_results.items()):
+        compilers.update(value)
+
+    dpoints_raw = []
 
     for key, value in sorted(bench_results.items()):
         normalize_val = value.get('clang', {}).get('mean')
@@ -112,21 +104,14 @@ if __name__ == "__main__":
             print('ignore  benchmark!!!')
             continue  # Ignore benchmark
 
-        x.append(key.replace('_test', ''))
-        for compiler in y:
+        for compiler in compilers:
             mean = value.get(compiler, {}).get('mean', 0)
             std_dev = value.get(compiler, {}).get('std_dev')
-            mean_normalized = mean / normalize_val
 
-            if std_dev:
-                std_dev_normalized = std_dev / normalize_val
-            else:
-                std_dev_normalized = None
+            mean_normalized = mean / normalize_val
+            std_dev_normalized = std_dev / normalize_val if std_dev else None
 
             dpoints_raw.append([compiler, key, mean_normalized, std_dev_normalized])
-
-            y[compiler].append(mean_normalized)
-            y_error[compiler].append(std_dev_normalized)
 
     dpoints = np.array(dpoints_raw)
 
