eea6b3d8c74c75c9721160f9883fcbc27903f7e5
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2018-03-30 12:06:16 +0200

    Change number of runs

--- make_test/richgel999-miniz_test.c
+++ make_test/richgel999-miniz_test.c
@@ -7,26 +7,24 @@
 #define LOREM_IPSUM100 LOREM_IPSUM10 LOREM_IPSUM10 LOREM_IPSUM10 LOREM_IPSUM10 LOREM_IPSUM10 LOREM_IPSUM10 LOREM_IPSUM10 LOREM_IPSUM10 LOREM_IPSUM10 LOREM_IPSUM10
 #define LOREM_IPSUM1000 LOREM_IPSUM100 LOREM_IPSUM100 LOREM_IPSUM100 LOREM_IPSUM100 LOREM_IPSUM100 LOREM_IPSUM100 LOREM_IPSUM100 LOREM_IPSUM100 LOREM_IPSUM100 LOREM_IPSUM100
 
-BENCHMARK(richgel999, miniz, 10, 1) {
+BENCHMARK(richgel999, miniz, 100, 1) {
     size_t cmp_len = 0;
 
     const char *p = LOREM_IPSUM1000;
     size_t uncomp_len = strlen(p);
 
-    for(int i = 0; i < 10; i++) {
-        void *pComp_data = tdefl_compress_mem_to_heap(p, uncomp_len, &cmp_len, TDEFL_WRITE_ZLIB_HEADER);
-        if (!pComp_data) {
-            free(pComp_data);
-            abort();
-        }
-
-        size_t decomp_len = 0;
-        void *pDecomp_data = tinfl_decompress_mem_to_heap(pComp_data, cmp_len, &decomp_len,
-                                                          TINFL_FLAG_PARSE_ZLIB_HEADER);
-
+    void *pComp_data = tdefl_compress_mem_to_heap(p, uncomp_len, &cmp_len, TDEFL_WRITE_ZLIB_HEADER);
+    if (!pComp_data) {
         free(pComp_data);
-        free(pDecomp_data);
+        abort();
     }
+
+    size_t decomp_len = 0;
+    void *pDecomp_data = tinfl_decompress_mem_to_heap(pComp_data, cmp_len, &decomp_len,
+                                                      TINFL_FLAG_PARSE_ZLIB_HEADER);
+
+    free(pComp_data);
+    free(pDecomp_data);
 }
 
 int main(int argc, char** argv) {
