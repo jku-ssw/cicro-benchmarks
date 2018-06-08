a329256837db3db9ffff1d50c832394a810f02b1
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2018-06-08 10:06:37 +0200

    use enviroment variables in all configs

--- configs/lli.py
+++ configs/lli.py
@@ -1,4 +1,4 @@
 def lli_executor(filepath, workdir, **kwargs):
     return wllvm_executor(filepath, workdir, '${LLI}', **kwargs)
 
-harness.add_runtime('lli-O3', {"CC": "wllvm", "AS": "wllvm", "CFLAGS": "-Wno-everything -O3"}, make_func=wllvm_make, exec_func=lli_executor)
+harness.add_runtime('lli-O3', {"CC": "$(WLLVM_DIR)/wllvm", "AS": "$(WLLVM_DIR)/wllvm", "CFLAGS": "-Wno-everything -O3"}, make_func=wllvm_make, exec_func=lli_executor)
