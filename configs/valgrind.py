82b13da5f406edb35c38ad2104c545cbe7ca4e7b
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2018-07-03 09:24:26 +0200

    Reformat configs directory and do some basic checks with linter

--- configs/valgrind.py
+++ configs/valgrind.py
@@ -2,6 +2,7 @@
 import subprocess
 import json
 
+
 def execute_binary_analysis_tool(filepath, workdir, tool, **kwargs):
     args = tool + [filepath, '--output=json'] + kwargs.get('exec_args', '').split(' ')
     with subprocess.Popen(args, cwd=workdir, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as process:
@@ -19,15 +20,19 @@ def execute_binary_analysis_tool(filepath, workdir, tool, **kwargs):
             logger.error('invalid benchmark result: \'%s\'', stdout.decode('utf-8'))
             raise
 
+
 def valgrind_executor(filepath, workdir, **kwargs):
     return execute_binary_analysis_tool(filepath, workdir, ['valgrind'], **kwargs)
+
 
 def callgrind_executor(filepath, workdir, **kwargs):
     return execute_binary_analysis_tool(filepath, workdir, ['valgrind', '--tool=callgrind'], **kwargs)
 
+
 def drmemory_executor(filepath, workdir, **kwargs):
     return execute_binary_analysis_tool(filepath, workdir, ['{$DR_MEMORY}', '--'], **kwargs)
 
-harness.add_runtime('valgrind-O3', {"CC": "${CLANG}", "AS": "${CLANG}", "CFLAGS": "-Wno-everything -O3", "PAPI":0}, exec_func=valgrind_executor)
-harness.add_runtime('callgrind-O3', {"CC": "${CLANG}", "AS": "${CLANG}", "CFLAGS": "-Wno-everything -O3", "PAPI":0}, exec_func=callgrind_executor)
+
+harness.add_runtime('valgrind-O3', {"CC": "${CLANG}", "AS": "${CLANG}", "CFLAGS": "-Wno-everything -O3", "PAPI": 0}, exec_func=valgrind_executor)
+harness.add_runtime('callgrind-O3', {"CC": "${CLANG}", "AS": "${CLANG}", "CFLAGS": "-Wno-everything -O3", "PAPI": 0}, exec_func=callgrind_executor)
 harness.add_runtime('drmemory-O3', {"CC": "${CLANG}", "AS": "${CLANG}", "CFLAGS": "-Wno-everything -O3"}, exec_func=drmemory_executor)
