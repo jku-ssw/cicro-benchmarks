6352a77da4c68fcc444c4ad9bbc8e0239d9e6a7e
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2018-07-13 11:12:34 +0200

    Fix comparison of unitinitialized bytes and memleak in antirez-otree_test.c

--- benchmarks/antirez-otree_test.c
+++ benchmarks/antirez-otree_test.c
@@ -11,17 +11,16 @@
 char *filename = "btree";
 
 BENCHMARK(otree, alloc_free, 10, 1) {
-    struct btree *bt;
     int count = 10000;
     int retval;
 
-    bt = btree_open(NULL, filename, BTREE_CREAT);
+    struct btree *bt = btree_open(NULL, filename, BTREE_CREAT);
     btree_clear_flags(bt, BTREE_FLAG_USE_WRITE_BARRIER);
     assert(bt != NULL);
 
     for (int j = 0; j < count; j++) {
-        char key[16];
-        snprintf(key, 16, "%d", j);
+        char key[BTREE_HASHED_KEY_LEN] = {0};
+        snprintf(key, BTREE_HASHED_KEY_LEN, "%d", j);
         retval = btree_add(bt, (unsigned char *)key, (unsigned char *)key, strlen(key), 1);
         assert(retval == 0);
     }
@@ -29,17 +28,19 @@ BENCHMARK(otree, alloc_free, 10, 1) {
     uint64_t voff;
     uint32_t datalen;
 
-    char key[16];
-    snprintf(key, 16, "%d", 1234);
+    char key[BTREE_HASHED_KEY_LEN] = {0};
+    snprintf(key, BTREE_HASHED_KEY_LEN, "%d", 1234);
     retval = btree_find(bt, (unsigned char*)key, &voff);
     assert(retval == 0);
 
     btree_alloc_size(bt, &datalen, voff);
     assert(datalen == 4);
+
     char *data = malloc(datalen+1);
     btree_pread(bt,(unsigned char*)data, datalen, voff);
     data[datalen] = '\0';
     assert(strcmp(data, "1234") == 0);
+    free(data);
 
     btree_close(bt);
 }
