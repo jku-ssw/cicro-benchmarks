84dcdaf48cead78c4d651a6bf8f75e6abbaf3787
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2018-07-23 14:27:25 +0200

    Add some improvements to the plot script
    * Ability to plot IPC when only some of the benchmarks have the data
    * Use benchmark name instead of their seperates name and fixture
    * Addtional plots to visualize differences as barchart
    * Make x-labels readable
    * Overwrite some label names

--- tools/plot.R
+++ tools/plot.R
@@ -61,12 +61,13 @@ df$config %>% unique()
 
 # create a long table to allow the following operations
 df_long = df %>%
-  gather(key=metric_name, value=value, -fixture, -name, -harness, -config) %>%
+  mutate(benchmark=paste(fixture, name, sep=".")) %>%
+  gather(key=metric_name, value=value, -fixture, -name, -benchmark, -harness, -config) %>%
   filter(!is.na(value))
 
 # calculate mean and sum over all runs
 df_long_summary = df_long %>%
-  group_by(fixture, name, config, metric_name) %>%
+  group_by(benchmark, config, metric_name) %>%
   summarise(runs=n(), value_mean=mean(value), value_sum=sum(value)) %>%
   ungroup()
 
@@ -74,8 +75,9 @@ df_long_summary = df_long %>%
 if('PAPI_TOT_INS' %in% df_long_summary$metric_name && 'PAPI_TOT_CYC' %in% df_long_summary$metric_name ) {
   print("plot instructions per cycle")
   df_long_baseline_ipc = df_long_summary %>%
-    group_by(fixture, name, config) %>%
-    transmute(ipc=value_mean[metric_name=='PAPI_TOT_INS']/value_mean[metric_name=='PAPI_TOT_CYC']) %>%
+    group_by(benchmark, config) %>%
+    filter('PAPI_TOT_INS' %in% metric_name && 'PAPI_TOT_CYC' %in% metric_name) %>%
+    transmute(ipc=value_mean[metric_name=='PAPI_TOT_INS'] / value_mean[metric_name=='PAPI_TOT_CYC']) %>%
     unique() %>%
     ungroup()
 
@@ -84,10 +86,13 @@ if('PAPI_TOT_INS' %in% df_long_summary$metric_name && 'PAPI_TOT_CYC' %in% df_lon
     ggtitle("instructions per cycle")
   print(p)
 
-  p2 = ggplot(df_long_baseline_ipc, aes(fill=ipc, x=name, y=config)) +
+  p2 = ggplot(df_long_baseline_ipc, aes(fill=ipc, x=benchmark, y=config)) +
     geom_tile(aes(fill = ipc), colour = "white") +
     scale_fill_gradient(low = "white", high = "steelblue", na.value = "grey50", trans="log1p", guide="legend") +
-    ggtitle("instructions per cycle")
+    scale_x_discrete('benchmark', expand = c(0, 0)) +
+    scale_y_discrete('runtime', expand = c(0, 0)) +
+    ggtitle("instructions per cycle") +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
   print(p2)
 }
 
@@ -96,8 +101,8 @@ if('PAPI_TOT_INS' %in% df_long_summary$metric_name) {
   print("plot instruction mix")
   df_long_baseline_papi = df_long_summary  %>%
     filter(grepl('^PAPI_.*_INS$', metric_name)) %>%
-    group_by(fixture, name, config) %>%
-    mutate(total_instruction_mean=value_mean[metric_name=='PAPI_TOT_INS' ]) %>%
+    group_by(benchmark, config) %>%
+    mutate(total_instruction_mean=value_mean[metric_name=='PAPI_TOT_INS']) %>%
     filter(metric_name!='PAPI_TOT_INS') %>%
     mutate(instruction_percent = value_mean/total_instruction_mean) %>%
     select(-runs, -value_mean, -value_sum, -total_instruction_mean) %>%
@@ -117,7 +122,7 @@ if('PAPI_TOT_INS' %in% df_long_summary$metric_name) {
 
 # add baseline to allow plotting relative to baseline
 df_long_baseline = df_long_summary  %>%
-  group_by(fixture, name, metric_name) %>%
+  group_by(benchmark, metric_name) %>%
   mutate(baseline=value_mean[config==BASE_RUNTIME ], baseline_runs=runs[config==BASE_RUNTIME ]) %>%
   ungroup()
 
@@ -125,43 +130,53 @@ for(plot_name in df_long_baseline$metric_name %>% unique()) {
   print(paste("plot comparison of:", plot_name, " "))
   p <- ggplot(df_long_baseline %>% filter(metric_name == plot_name), aes(x=config, y=value_mean/baseline)) +
     geom_boxplot(notch = TRUE, varwidth=TRUE) +
-    scale_x_discrete(name = "runtimes") +
-    scale_y_continuous(name = "factor") +
+    scale_x_discrete("runtime") +
+    scale_y_continuous("factor") +
     ggtitle(plot_name)
   print(p)
 
   my_breaks = c(0.1, 0.5, 1, 1.5, 2, 5, 10, 50, 100, 200, 500, 1000)
-  p2 = ggplot(df_long_baseline %>% filter(metric_name == plot_name), aes(x=name, fill=value_mean/baseline, y=config)) +
+  p2 = ggplot(df_long_baseline %>% filter(metric_name == plot_name), aes(x=benchmark, fill=value_mean/baseline, y=config)) +
     geom_tile(aes(fill = value_mean/baseline), colour = "white") +
     scale_fill_gradient(low = "white", high = "steelblue", na.value = "grey50", trans="log1p", breaks=my_breaks, labels=my_breaks, guide="legend") +
-    ggtitle(plot_name)
+    scale_x_discrete('benchmark', expand = c(0, 0)) +
+    scale_y_discrete('runtime', expand = c(0, 0)) +
+    ggtitle(plot_name) +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
   print(p2)
+
+  p_barchart = ggplot(df_long_baseline %>% filter(metric_name == plot_name), aes(x=benchmark, y=value_mean/baseline, fill=config)) +
+    geom_bar(stat="identity", position=position_dodge()) +
+    scale_x_discrete('benchmark', expand = c(0, 0)) +
+    scale_y_continuous('runtime', expand = c(0, 0, 0.05, 0)) +
+    ggtitle(plot_name) +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
+  print(p_barchart)
 }
 
 
 # interactive plot is also possible:
 
 #library(plotly)
-#p_boxplot <- p + geom_point(aes(text=paste(fixture, name, sep=".")), colour="grey", alpha=1/4)
+#p_boxplot <- p + geom_point(aes(text=benchmark), colour="grey", alpha=1/4)
 #ggplotly(p_boxplot, dynamicTicks=TRUE)
-
-
-# interactive barchart to visualize all benchmarks with all runtimes
-
-#p_barchart = ggplot(df_long_baseline %>% filter(metric_name == plot_name), aes(x=name, y=value_mean/baseline, fill=config)) +
-#  geom_bar(stat="identity", position=position_dodge())
-#ggplotly(p_barchart, dynamicTicks=TRUE)
 
 
 # matrix visualisation of all plots, to show patterns and outliers
 
 # calculate absolute runtime
-ggplot(df_long_summary %>% filter(metric_name == 'duration'), aes(x=name, fill=value_sum/1000000, y=config)) +
+ggplot(df_long_summary %>% filter(metric_name == 'duration'), aes(x=benchmark, fill=value_sum/1000000, y=config)) +
   geom_tile(aes(fill = value_sum/1000000), colour = "white") +
   scale_fill_gradient(low = "white", high = "steelblue", na.value = "grey50", trans="log1p", guide="legend") +
-  ggtitle('absolute runtime in seconds')
+  scale_x_discrete('benchmark', expand = c(0, 0)) +
+  scale_y_discrete('runtime', expand = c(0, 0)) +
+  ggtitle('absolute runtime in seconds') +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
 
-ggplot(df_long_baseline %>% filter(metric_name == 'duration'), aes(x=name, fill=runs/baseline_runs, y=config)) +
+ggplot(df_long_baseline %>% filter(metric_name == 'duration'), aes(x=benchmark, fill=runs/baseline_runs, y=config)) +
   geom_tile(aes(fill = runs/baseline_runs), colour = "white") +
   scale_fill_gradient(low = "white", high = "steelblue", na.value = "grey50", guide="legend") + #, trans="log1p"
-  ggtitle('number of runs relative to baseline')
+  scale_x_discrete('benchmark', expand = c(0, 0)) +
+  scale_y_discrete('runtime', expand = c(0, 0)) +
+  ggtitle('number of runs relative to baseline') +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
