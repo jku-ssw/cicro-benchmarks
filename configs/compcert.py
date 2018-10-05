bb8cdc360e2dca2f1c0142a20524e471abe880aa
Manuel Rigger <manuel.rigger@jku.at>
2018-10-05 15:09:46 +0200

    Improve the robustness of the CompCert configuration

--- configs/compcert.py
+++ configs/compcert.py
@@ -2,4 +2,4 @@ from functools import partial
 
 compcert_kwargs = {'build_system_func': partial(build_system_executor, cc_version='--version', as_version='--version')}
 
-harness.add_runtime('compcert', {"CC": "${COMPCERT}", "AS": "as", "CFLAGS": "-fstruct-passing -fbitfields"}, **compcert_kwargs)
+harness.add_runtime('compcert', {"CC": "${COMPCERT}", "AS": "as", "CFLAGS": "-fall"}, **compcert_kwargs)
