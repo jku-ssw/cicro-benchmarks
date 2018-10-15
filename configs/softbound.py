6c7dbf5fcd65a3134831475ae701fe0fbe0cf996
Manuel Rigger <manuel.rigger@jku.at>
2018-10-15 17:33:35 +0200

    Add a docker image for SoftBound+CETS

--- configs/softbound.py
+++ configs/softbound.py
@@ -1,1 +1,1 @@
-harness.add_runtime('softbound-O3', {"CC": "${SOFTBOUND_CLANG}", "AS": "${SOFTBOUND_CLANG}", "CFLAGS": "-O3 -fsoftboundcets -L${SOFTBOUND_RUNTIME_DIR} -lsoftboundcets_rt", "LDFLAGS": ""})
+harness.add_runtime('softboundcets-O3', {"CC": "${CLANG}", "AS": "${CLANG}", "CFLAGS": "-O3 -fsoftboundcets -L${SOFTBOUND_RUNTIME_DIR} -lsoftboundcets_rt", "LDFLAGS": ""})
