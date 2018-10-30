f8b93553a8f9801074de4a580c57f34b27d24628
Manuel Rigger <manuel.rigger@jku.at>
2018-10-30 13:11:18 +0100

    Update the generation of the figures

--- configs/compcert.py
+++ configs/compcert.py
@@ -2,6 +2,6 @@ from functools import partial
 
 compcert_kwargs = {'build_system_func': partial(build_system_executor, cc_version='--version', as_version='--version')}
 
-harness.add_runtime('compcert', {"CC": "${COMPCERT}", "AS": "as", "CFLAGS": "-fall"}, **compcert_kwargs)
+harness.add_runtime('compcert-O', {"CC": "${COMPCERT}", "AS": "as", "CFLAGS": "-fall -O"}, **compcert_kwargs)
 harness.add_runtime('compcert-O0', {"CC": "${COMPCERT}", "AS": "as", "CFLAGS": "-fall -O0"}, **compcert_kwargs)
 harness.add_runtime('compcert-Os', {"CC": "${COMPCERT}", "AS": "as", "CFLAGS": "-fall -Os"}, **compcert_kwargs)
