3947a5c7cb0087287dfa3a95074663d1c5f49c89
Manuel Rigger <manuel.rigger@jku.at>
2018-11-02 19:04:59 -0400

    Add a docker container for libdislocator

--- configs/valgrind.py
+++ configs/valgrind.py
@@ -96,3 +96,4 @@ harness.add_runtime('diehard-O3', {"CC": "${CLANG}", "AS": "${CLANG}", "CFLAGS":
 harness.add_runtime('jemalloc-O3', {"CC": "${CLANG}", "AS": "${CLANG}", "CFLAGS": "-Wno-everything -O3"})
 harness.add_runtime('hoard-O3', {"CC": "${CLANG}", "AS": "${CLANG}", "CFLAGS": "-Wno-everything -O3"})
 harness.add_runtime('freeguard-O3', {"CC": "${CLANG}", "AS": "${CLANG}", "CFLAGS": "-Wno-everything -O3"})
+harness.add_runtime('libdislocator-O3', {"CC": "${CLANG}", "AS": "${CLANG}", "CFLAGS": "-Wno-everything -O3"})
