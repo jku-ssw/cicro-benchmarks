c56f0330fe036e100fd0c51bd30181be8ad577c6
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2018-11-09 10:34:19 +0100

    Move softbound to LLVM 3.8

--- configs/softbound.py
+++ configs/softbound.py
@@ -1,1 +1,1 @@
-harness.add_runtime('softboundcets-O3', {"CC": "${CLANG}", "AS": "${CLANG}", "CFLAGS": "-O3 -fsoftboundcets -L${SOFTBOUND_RUNTIME_DIR}", "LDFLAGS": ""})
+harness.add_runtime('softboundcets-O3', {"CC": "${CLANG}", "AS": "${CLANG}", "CFLAGS": "-O3 -fsoftboundcets -L${SOFTBOUND_RUNTIME_DIR}", "LDFLAGS": "-lm -lrt -lsoftboundcets_rt"})
