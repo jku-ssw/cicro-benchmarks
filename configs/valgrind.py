4e1bf0bc28de00fa9230d0f69b8d7d0fb09e4753
Manuel Rigger <manuel.rigger@jku.at>
2019-03-17 21:11:43 +0100

    Add Dockerfiles for /usr/bin/time and GCC 4.9

--- configs/valgrind.py
+++ configs/valgrind.py
@@ -27,6 +27,18 @@ def ltrace_build_system_executor(make_env):
 
     return result
 
+def time_build_system_executor(make_env):
+    result = build_system_executor(make_env, cc_version='--version', as_version='--version')
+
+    with subprocess.Popen(['/usr/bin/time', '--version'], stdout=subprocess.PIPE) as p:
+        stdout, _ = p.communicate(timeout=1)
+
+        stdout_decoded = stdout.decode('utf-8').rstrip() if stdout else None
+        if p.returncode == 0 and stdout_decoded:
+            result['USR-BIN-TIME_version'] = stdout_decoded
+
+    return result
+
 
 def strace_build_system_executor(make_env):
     result = build_system_executor(make_env, cc_version='--version', as_version='--version')
@@ -110,6 +122,9 @@ def strace_executor(filepath, workdir, exec_args, **kwargs):
 def ltrace_executor(filepath, workdir, exec_args, **kwargs):
     return execute_binary_analysis_tool(filepath, workdir, ['ltrace', '-o', 'test'], exec_args, **kwargs)
 
+def time_executor(filepath, workdir, exec_args, **kwargs):
+    return execute_binary_analysis_tool(filepath, workdir, ['/usr/bin/time', '--verbose'], exec_args, **kwargs)
+
 
 valgrind_kwargs = {'build_system_func': valgrind_build_system_executor, 'exec_func': valgrind_executor}
 callgrind_kwargs = {'build_system_func': valgrind_build_system_executor, 'exec_func': callgrind_executor}
@@ -117,6 +132,7 @@ drmemory_kwargs = {'build_system_func': drmemory_build_system_executor, 'exec_fu
 qemu_kwargs = {'build_system_func': qemu_build_system_executor, 'exec_func': qemu_executor}
 strace_kwargs = {'build_system_func': strace_build_system_executor, 'exec_func': strace_executor}
 ltrace_kwargs = {'build_system_func': ltrace_build_system_executor, 'exec_func': ltrace_executor}
+time_kwargs = {'build_system_func': time_build_system_executor, 'exec_func': time_executor}
 
 harness.add_runtime('valgrind-O3', {"CC": "${CLANG}", "AS": "${CLANG}", "CFLAGS": "-Wno-everything -O3", "PAPI": 0}, **valgrind_kwargs)
 harness.add_runtime('callgrind-O3', {"CC": "${CLANG}", "AS": "${CLANG}", "CFLAGS": "-Wno-everything -O3", "PAPI": 0}, **callgrind_kwargs)
@@ -134,3 +150,4 @@ harness.add_runtime('dlmalloc-O3', {"CC": "${CLANG}", "AS": "${CLANG}", "CFLAGS"
 harness.add_runtime('tlsf-O3', {"CC": "${CLANG}", "AS": "${CLANG}", "CFLAGS": "-Wno-everything -O3"})
 harness.add_runtime('strace-O3', {"CC": "${CLANG}", "AS": "${CLANG}", "CFLAGS": "-Wno-everything -O3"}, **strace_kwargs)
 harness.add_runtime('ltrace-O3', {"CC": "${CLANG}", "AS": "${CLANG}", "CFLAGS": "-Wno-everything -O3"}, **ltrace_kwargs)
+harness.add_runtime('usr-bin-time-O3', {"CC": "${CLANG}", "AS": "${CLANG}", "CFLAGS": "-Wno-everything -O3", "PAPI": 0}, **valgrind_kwargs)
