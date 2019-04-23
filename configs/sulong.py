ac76bec3f4b7b2db75b95cf22625b095caac938c
Josef Eisl <zapster@zapster.cc>
2019-04-23 17:50:22 +0200

    Use sulong from graalvm release rc15

--- configs/sulong.py
+++ configs/sulong.py
@@ -7,7 +7,7 @@ from functools import partial
 def graalvm_build_system_executor(make_env):
     result = build_system_executor(make_env, cc_version='--version', as_version='--version')
 
-    with subprocess.Popen([os.path.expandvars('$GRAALVM_DIR/lli'), '--version'], stdout=subprocess.PIPE) as p:
+    with subprocess.Popen([os.path.expandvars('${GRAALVM_DIR}/bin/lli'), '--version'], stdout=subprocess.PIPE) as p:
         stdout, _ = p.communicate(timeout=1)
 
         stdout_decoded = stdout.decode('utf-8').rstrip() if stdout else None
@@ -32,7 +32,7 @@ def sulong_build_system_executor(make_env):
 
 
 def sulong_graalvm_executor(filepath, workdir, exec_args, warmup, **kwargs):
-    return wllvm_executor(filepath, workdir, '$GRAALVM_DIR/lli', exec_args + ["--warmup={}".format(warmup)], **kwargs)
+    return wllvm_executor(filepath, workdir, os.path.expandvars('${GRAALVM_DIR}/bin/lli'), exec_args + ["--warmup={}".format(warmup)], **kwargs)
 
 
 def sulong_latest_executor(filepath, workdir, exec_args, warmup, **kwargs):
@@ -51,12 +51,10 @@ sulong_env = {
 graalvm_kwargs = {'build_system_func': graalvm_build_system_executor}
 sulong_kwargs = {'build_system_func': sulong_build_system_executor}
 
-harness.add_runtime('sulong-graalvm', sulong_env, make_func=wllvm_make, exec_func=partial(sulong_graalvm_executor, warmup=100), **graalvm_kwargs)
-
-harness.add_runtime('sulong-warmup-0', sulong_env, make_func=wllvm_make, exec_func=partial(sulong_latest_executor, warmup=0), **sulong_kwargs)
-harness.add_runtime('sulong-warmup-1', sulong_env, make_func=wllvm_make, exec_func=partial(sulong_latest_executor, warmup=1), **sulong_kwargs)
-harness.add_runtime('sulong-warmup-2', sulong_env, make_func=wllvm_make, exec_func=partial(sulong_latest_executor, warmup=2), **sulong_kwargs)
-harness.add_runtime('sulong-warmup-10', sulong_env, make_func=wllvm_make, exec_func=partial(sulong_latest_executor, warmup=10), **sulong_kwargs)
-harness.add_runtime('sulong-warmup-100', sulong_env, make_func=wllvm_make, exec_func=partial(sulong_latest_executor, warmup=100), **sulong_kwargs)
-harness.add_runtime('sulong-warmup-1000', sulong_env, make_func=wllvm_make, exec_func=partial(sulong_latest_executor, warmup=1000), **sulong_kwargs)
-harness.add_runtime('sulong-warmup-10000', sulong_env, make_func=wllvm_make, exec_func=partial(sulong_latest_executor, warmup=10000), **sulong_kwargs)
+harness.add_runtime('sulong-warmup-0', sulong_env, make_func=wllvm_make, exec_func=partial(sulong_graalvm_executor, warmup=0), **graalvm_kwargs)
+harness.add_runtime('sulong-warmup-1', sulong_env, make_func=wllvm_make, exec_func=partial(sulong_graalvm_executor, warmup=1), **graalvm_kwargs)
+harness.add_runtime('sulong-warmup-2', sulong_env, make_func=wllvm_make, exec_func=partial(sulong_graalvm_executor, warmup=2), **graalvm_kwargs)
+harness.add_runtime('sulong-warmup-10', sulong_env, make_func=wllvm_make, exec_func=partial(sulong_graalvm_executor, warmup=10), **graalvm_kwargs)
+harness.add_runtime('sulong-warmup-100', sulong_env, make_func=wllvm_make, exec_func=partial(sulong_graalvm_executor, warmup=100), **graalvm_kwargs)
+harness.add_runtime('sulong-warmup-1000', sulong_env, make_func=wllvm_make, exec_func=partial(sulong_graalvm_executor, warmup=1000), **graalvm_kwargs)
+harness.add_runtime('sulong-warmup-10000', sulong_env, make_func=wllvm_make, exec_func=partial(sulong_graalvm_executor, warmup=10000), **graalvm_kwargs)
